f1863ecb79fbae74d8069fdca938910744c3c2bd
Filip <49274205+Tigerly1@users.noreply.github.com>
2022-03-17 13:40:14 +0100

    Update, low acc before

--- lab2.py
+++ lab2.py
@@ -1,135 +1,94 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[ ]:
 
 
 from sklearn.datasets import fetch_openml
 import numpy as np
+import pandas as pd
 mnist = fetch_openml('mnist_784', version=1)
 
 
-# In[8]:
+# ### Poprawne zbiory danych +97% acc
 
-
-print((np.array(mnist.data.loc[42]).reshape(28, 28) > 0).astype(int))
-
-
-# In[27]:
-
-
-import pandas as pd
-y = pd.Series(mnist.target.astype(np.uint8))
-X = mnist.data
-y = y.sort_values(ascending=True)
-
-
-# In[28]:
-
-
-y.index
-
-
-# In[30]:
-
-
-X.reindex(y.index)
-
-
-# In[30]:
-
-
-X.reindex(y.index)
-
-
-# In[31]:
-
-
-X_train, X_test = X[:56000], X[56000:]
-y_train, y_test = y[:56000], y[56000:]
-print(X_train.shape, y_train.shape)
-print(X_test.shape, y_test.shape)
-
-
-# In[33]:
-
-
-y_train, y_test
-
-
-# In[36]:
+# In[37]:
 
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X, y = mnist.data, mnist.target.astype(np.uint8)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 
 
-# In[41]:
+# In[38]:
 
 
 pd.unique(y_train.sort_values()), pd.unique(y_test.sort_values())
 
 
-# In[45]:
+# In[40]:
 
 
 from sklearn.linear_model import SGDClassifier
 y_train_0 = (y_train == 0)
 y_test_0 = (y_test == 0)
-
 sgd_clf = SGDClassifier(random_state=42)
 sgd_clf.fit(X_train, y_train_0)
 
 
-# In[46]:
+# In[42]:
 
 
-a = sgd_clf.predict(X_train)
-b = sgd_clf.predict(X_test)
+sgd_clf.score(X_train, y_train_0), sgd_clf.score(X_test, y_test_0)
 
 
-# In[72]:
+# In[44]:
 
 
+import pickle
 pickle.dump([sgd_clf.score(X_train, y_train_0), sgd_clf.score(X_test, y_test_0)], open("sgd_acc.pkl", "wb"))
 
 
-# In[75]:
+# In[50]:
 
 
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import confusion_matrix
 
 
-# In[79]:
+# In[47]:
 
 
 y_train_score = cross_val_score(sgd_clf, X_train, y_train_0, cv=3, n_jobs=-1)
+y_train_score
 
 
-# In[80]:
+# In[48]:
 
 
 pickle.dump(y_train_score, open("sgd_cva.pkl", "wb"))
 
 
-# In[81]:
+# In[52]:
 
 
 sgd_clf_all = SGDClassifier(random_state=42)
 sgd_clf_all.fit(X_train, y_train)
 
 
-# In[ ]:
+# In[53]:
 
 
 y_train_predict_all = cross_val_predict(sgd_clf_all, X_train, y_train, cv=3, n_jobs=-1)
+y_train_predict_all
 
 
-# In[ ]:
+# In[54]:
 
 
 conf_mx = confusion_matrix(y_train, y_train_predict_all)
+conf_mx
 
 
 # In[ ]:
